ed60c19a362b7e8fbe4fb88cecddfca497cad5b2
hossensumon071 <55638643+hossensumon071@users.noreply.github.com>
2022-12-02 18:36:52 +0600

    style product cart and position button

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,11 +3,19 @@ import './Product.css';
 
 const Product = (props) => {
   // console.log(props.product)
-  const {name, img, seller, price, ratings} = props.product
+  const {name, img, seller, price, star} = props.product
   return (
     <div className="product">
       <img src={img} alt="" />
-      <p className='product-name'>{name}</p>
+      <div className="product-info">
+        <p className='product-name'>{name}</p>
+        <p>Price: ${price}</p>
+        <p><small>Seller: {seller}</small></p>
+        <p><small>Ratings: {star} stars</small></p>
+      </div>
+      <button className="btn-cart">
+        Add To Cart
+      </button>
     </div>
   );
 };
