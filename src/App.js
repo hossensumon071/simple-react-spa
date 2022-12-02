36a6e56f1eb95bb291109ad7db8c0b1844d65831
hossensumon071 <55638643+hossensumon071@users.noreply.github.com>
2022-12-02 17:15:51 +0600

    created shop compoent and load data also set state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './index.css'
 import Header from "./components/Header/Header";
+import Shop from './components/Shop/Shop';
 
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Shop></Shop>
     </div>
   );
 }
